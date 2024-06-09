ed9d4dfa88239213fe5da4f79f0504253d9fcd7a
Aubin Djou <116364607+acelest@users.noreply.github.com>
2024-06-09 11:13:40 +0100

    url

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CandidatVotesAPIView, DisciplineViewSet, CandidatViewSet, VoteViewSet,InitiatePaymentAPIView, VerifyPaymentAPIView
+from .views import CandidatVotesAPIView, DisciplineViewSet, CandidatViewSet, VoteViewSet, InitiatePaymentAPIView, VerifyPaymentAPIView
 from .views import CandidatListAPIView
 
 router = DefaultRouter()
