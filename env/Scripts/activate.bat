f72e2791d333468275b0a12a449f5832c2ebb4c4
asbilim <bilimdepaullilian@gmail.com>
2024-06-11 13:35:57 +0100

    j'ai elmevé les trucs inutiles , et ajouter d'autres variables d'environnement , j'ai configuré les images et j'ai turn debug to True

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\aubin\code\python\thesummershow-backend\env
+set VIRTUAL_ENV=C:\Users\bilim\Documents\backend\django\summershow-backend\env
 
 if not defined PROMPT set PROMPT=$P$G
 
